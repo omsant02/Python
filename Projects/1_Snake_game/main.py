0710f1c598bfaaf16946bc7e1d5123bfb307c4a5
omsant02 <omsantoshwar02@gmail.com>
2024-01-13 13:18:47 +0530

    add highscore value retention

--- Projects/1_Snake_game/main.py
+++ Projects/1_Snake_game/main.py
@@ -36,8 +36,9 @@ while game_is_on:
 
       #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
 
 
       #Detect collision with tail
@@ -50,8 +51,9 @@ while game_is_on:
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
+
 
 
 screen.exitonclick()
